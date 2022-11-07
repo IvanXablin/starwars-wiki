7fa9071be44c1441a240ed7b637b6319e982372e
Ivan Kondratev <amdandpc67890@gmail.com>
2022-11-07 21:43:43 +0700

    init project

---
+++ src/components/header/Header.vue
@@ -0,0 +1,35 @@
+<template>
+<header class="header">
+  <img src="src/assets/images/logo.svg" />
+  <nav>
+    <a href="">Home</a>
+    <a href="">Characters</a>
+    <a href="">Planets</a>
+    <a href="">Starships</a>
+  </nav>
+</header>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 80px;
+  background: #212121;
+  opacity: 0.85;
+
+  img {
+    width: 20%;
+  }
+
+  a {
+    margin: 0 15px;
+    font-size: 17px;
+    text-decoration: none;
+    text-transform: uppercase;
+    font-family: 'Cairo', sans-serif;
+    color: #ffffff;
+  }
+}
+</style>
---
+++ src/layout/Layout.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Header from "../components/header/Header.vue";
+</script>
+
+<template>
+<Header/>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
