433c40d821e32234fc500b1e9cce98c2271b9e29
Ivan Kondratev <amdandpc67890@gmail.com>
2022-11-15 22:24:47 +0700

    fix docs

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,4 @@
-export const URL_LOGO_PLANET = "/src/assets/images/planeta.png";
-export const URL_LOGO_NAVE = "/src/assets/images/nave.png";
-export const URL_LOGO_HELMET = "/src/assets/images/stormtrooper.png";
+export const URL_LOGO_PLANET = "https://raw.githubusercontent.com/IvanXablin/starwars-wiki/master/src/assets/images/stormtrooper.png";
+export const URL_LOGO_NAVE = "https://raw.githubusercontent.com/IvanXablin/starwars-wiki/master/src/assets/images/nave.png";
+export const URL_LOGO_HELMET = "https://raw.githubusercontent.com/IvanXablin/starwars-wiki/master/src/assets/images/planeta.png";
+
